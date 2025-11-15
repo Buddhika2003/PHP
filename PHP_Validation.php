88497280d5f56981e7673b70d4824d7b9a56c1e3
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-11-15 15:37:56 +0530

    Add PHP form with input validation

--- PHP_Validation.php
+++ PHP_Validation.php
@@ -4,6 +4,23 @@
     <title>PHP Form Validation</title>
     </head>
     <body>
+        <?php
+            $name=$email=$website=$comment=$gender="";
+
+            if($_SERVER['REQUEST_METHOD'] == "POST"){
+                $name = htmlspecialchars($_POST['name']);
+                $email = htmlspecialchars($_POST['email']);
+                $website = htmlspecialchars($_POST['website']);
+                $comment = htmlspecialchars($_POST['comment']);
+                $gender = htmlspecialchars($_POST['gender']);
+                echo "<h2>Your Input:</h2>";
+                echo "Name: $name<br>"; 
+                echo "Email: $email<br>";
+                echo "Website: $website<br>";
+                echo "Comment: $comment<br>";
+                echo "Gender: $gender<br><br>";
+            }
+        ?>
         <form action="" method="post">
         Name : <input type="text" name="name" required><br><br>
         E-Mail : <input type="email" name="email" required><br><br>
