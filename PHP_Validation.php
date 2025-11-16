e6b8d084fb910ee12c0aefe1d339dc697d658b20
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-11-16 23:26:53 +0530

    Enhance input validation: name regex check, email format validation, and URL pattern validation

--- PHP_Validation.php
+++ PHP_Validation.php
@@ -34,11 +34,17 @@
                     $nameErr = "Name is required";
                 } else {
                     $name = test_input($_POST['name']);
+                    if(!preg_match("/^[a-zA-Z-' ]*$/",$name)){
+                        $nameErr = "Only letters and white space allowed";
+                    }
                 }
                 if(empty($_POST['email'])){
                     $emailErr = "Email is required";
                 }else{
                     $email = test_input($_POST['email']);
+                    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                        $emailErr = "Invalid email format";
+                    }
                 }
                 if(empty($_POST['gender'])){
                     $genderErr = "Gender is required";
@@ -49,12 +55,16 @@
                     $website = "";
                 }else{
                     $website = test_input($_POST['website']);
+                    if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)){
+                        $websiteErr = "Invalid URL";
+                    }
                 }
                 if(empty($_POST['comment'])){
                     $comment = "";
                 }else{
                     $comment = test_input($_POST['comment']);
                 }
+            }
         ?>
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
         Name : <input type="text" name="name">
